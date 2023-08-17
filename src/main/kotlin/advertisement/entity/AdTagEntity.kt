16745abb111dbf388dc84297dba7121d85355349
Aravinth Alvapillai <aravinth123@hotmail.com>
2023-08-17 18:59:18 +0200

    update github action file

--- src/main/kotlin/advertisement/entity/AdTagEntity.kt
+++ src/main/kotlin/advertisement/entity/AdTagEntity.kt
@@ -2,10 +2,12 @@ package advertisement.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 
 @Entity(name = "adtags")
 class AdTagEntity {
     @Column(name = "ad_id")
+    @Id
     var adId: Int = 0
 
     @Column(name = "tag_name")
--- src/main/kotlin/advertisement/entity/CategoryEntity.kt
+++ src/main/kotlin/advertisement/entity/CategoryEntity.kt
@@ -2,9 +2,11 @@ package advertisement.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 
 @Entity(name = "category")
 class CategoryEntity {
     @Column(name = "name")
+    @Id
     lateinit var name: String
 }
