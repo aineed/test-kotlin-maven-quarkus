b4e11c4b2c669e666c5090217e83d2230dc910f1
Aravinth Alvapillai <aravinth123@hotmail.com>
2023-08-17 12:15:05 +0200

    add entity and database connection

---
+++ src/test/kotlin/advertisement/service/AdServiceTest.kt
@@ -0,0 +1,14 @@
+package advertisement.service
+
+import org.junit.jupiter.api.Test
+
+class AdServiceTest {
+
+    @Test
+    fun should_map_from_category_domain_to_category(){
+       /* val category = Category("String")
+        val categoryEntity = adService.mapToCategoryEntity(category)
+
+        assertEquals(category.name, categoryEntity.name)*/
+    }
+}
